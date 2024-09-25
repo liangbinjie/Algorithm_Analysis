850838dc421e53db2634e937f9fd6c1a2f4ec9a9
Benji <67171031+liangbinjie@users.noreply.github.com>
2024-09-25 06:15:27 +0000

    small changes

--- Project2/main.c
+++ Project2/main.c
@@ -7,58 +7,59 @@ void processFile() {
 
 }
 
+// crea un archivo con el nombre <filename> y escribe la tabla de frecuencias
 void createFile(char *filename) {
     FILE *file;
     file = fopen(filename, "w");
     fprintf(file, "Hex | Symbol | Frecuency");
     for (int i=0; i<256;i++) {
         fprintf(file, "\n%3x |", i);
-        if (i < 32 || i > 125) {
+        if (i < 32 || i == 127) {
             fprintf(file, "        |");
         } else {
             fprintf(file, "   %c    |", i);
         }
-
     }
     fclose(file);
 }
 
+// imprime el contenido de un archivo <file>
 void printContent(FILE *file) {
-    char c;
-    while ((c = fgetc(file)) != EOF) {
-        printf("%c", c);
+    char buffer[256];
+    while (fgets(buffer, sizeof(buffer), file)) {
+        printf("%s", buffer);
     }
     fclose(file);
 }
 
 // ./main <file-frec> <file-1> <file-2> ... <file-n>
 int main(int argc, char* argv[]) {
+
     if (argc == 1) {
         // no se recibio ningun archivo
-        printf("No se ingreso un archivo para ejecutar el programa");
+        printf("No se ingreso un archivo para ejecutar el programa\n");
         return 0;
     }
 
     FILE *file;
     file = fopen(argv[1], "r");
-
     if (file == NULL) {
         // si no existe <file-frec>, lo crea
-        fclose(file);
+        createFile(argv[1]);
     }
-
-    createFile(argv[1]);
 
     if (argc == 2) {
         // se recibe solamente <file-frec>
         // mostrar contenido
         file = fopen(argv[1], "r");
         printContent(file);
+        printf("\n");
         return 0;
     }
 
     for (int i=2; i<argc; i++) {
-        printf(argv[i]);
+        printf("%s\n", argv[i]);
     } 
+
     return 0;
 }
