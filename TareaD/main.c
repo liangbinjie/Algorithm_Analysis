baa820d07e4690b7d9a770247d0660b1f20315bc
Benji <67171031+liangbinjie@users.noreply.github.com>
2024-10-24 16:18:24 +0000

    finished

--- TareaD/main.c
+++ TareaD/main.c
@@ -4,10 +4,6 @@
 #include <stdbool.h>
 #include <time.h>
 
-
-// Programar N_Queens
-// Programar MC_Queen
-
 long double solutions = 0;
 long double nodes = 1;
 long double promisingNodes = 1;
@@ -25,50 +21,51 @@ int promising(int row,int column) {
         i++;
     }
  
-    return segura; //no conflicts
+    return segura;
 }
 
 long double MC_Queens(int n) {
     int i, j, m;
     long double mprod, numnodes;
-    bool prom_children[n];  // Array to track promising children
-    int promising_indices[n];  // To store indices of promising nodes
+    bool prom_children[n]; 
+    int promising_indices[n];
 
     i = 0;
-    numnodes = 1;  // Start with the root node
-    m = 1;  // Initialize to 1 promising node for the root
-    mprod = 1;  // Product of promising nodes
+    numnodes = 1;  // siempre se comienza con el nodo raíz
+    m = 1;  // inicializar a 1 nodo promising para la raíz
+    mprod = 1;  // producto de nodos promising
 
     while (m != 0 && i != n) {
-        mprod = mprod * m;  // Multiply the current number of promising nodes
-        numnodes = numnodes + mprod * n;  // Add the nodes at this level
+        mprod = mprod * m;  // multiplicamos el número actual de nodos promising
+        numnodes = numnodes + mprod * n;  // agregamos los nodos en este nivel
 
-        i++;  // Move to the next row
-        m = 0;  // Reset number of promising nodes
+        i++;  // siguiente fila
+        m = 0;  // resetear el número de nodos promising
 
-        // Reset prom_children set and promising_indices array
+        // prom?children = conjunto vacio
         for (j = 0; j < n; j++) {
             prom_children[j] = false;
         }
 
         int promising_count = 0;
         for (j = 1; j <= n; j++) {
-            if (promising(i, j)) {  // Check if position (i, j) is promising
-                m++;  // Increase count of promising nodes
-                prom_children[j - 1] = true;  // Mark this column as promising
-                promising_indices[promising_count++] = j;  // Store the promising column
+            if (promising(i, j)) { 
+                m++;  // incrementar el conteo de nodos prometedores
+                prom_children[j - 1] = true;  // marcar esta columna como promising
+                promising_indices[promising_count++] = j;  // almacenar la columna promising
             }
         }
 
         if (m != 0) {
-            // Random selection from the promising nodes
+            // selecionar aleatoriamente de los nodos promising
             int random_index = rand() % promising_count;
-            j = promising_indices[random_index];  // Select a random promising column
-            board[i] = j;  // Place queen in the selected promising column
+            j = promising_indices[random_index]; 
+            board[i] = j; 
         }
     }
-    return numnodes;  // Return total number of nodes in the tree
+    return numnodes; 
 }
+
 
 void NQueens(int k, int n) {
     int i;
@@ -107,22 +104,23 @@ long double factorial_NQueens(int n) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <n>\n", argv[0]);
-        return 1;
-    } else if (argc > 2) {
-        printf("Usage: %s <n>\n", argv[0]);
-        return 1;
-    }
+    // if (argc != 2) {
+    //     printf("Usage: %s <n>\n", argv[0]);
+    //     return 1;
+    // } else if (argc > 2) {
+    //     printf("Usage: %s <n>\n", argv[0]);
+    //     return 1;
+    // }
 
-    int max = atoi(argv[1]);
+    // int max = atoi(argv[1]);
+
+    FILE *file = fopen("output.txt", "w");
+
 
     srand(time(NULL));
-
     printf("%-8s | %-15s | %-15s | %-15s | %-15s | %-10s | %s\n", 
            "N", "Exhaustive", "Factorial", "Nodes", "Promising Nodes", "Solutions", "MC_Queens");
-    
-    for (int n = 4; n <= max; n++) {
+    for (int n = 4; n <= 100; n++) {
         solutions = 0;
         nodes = 1;
         promisingNodes = 1;
@@ -142,7 +140,7 @@ int main(int argc, char *argv[]) {
             printf(" %-15.0Lf |", promisingNodes);                      // For "Promising Nodes" (rounded to 0 decimal places)
             printf(" %-10.0Lf |", solutions);                              // For "Solutions" (rounded to 0 decimal places)
             printf(" %.Lf\n", mc_nodes);                                    // For "MC_Queens" (rounded to 0 decimal places)
-        } else if (n < 21 && n >= 9) {
+        } else if (n < 16 && n >= 9) {
             printf("%-8d |", n);
             printf(" %-15.2e |", (double)exhaustive_NQueens(n));
             printf(" %-15.2e |", (double)factorial_NQueens(n));
